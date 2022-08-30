77a99522df68b51ba0ff9a1d7c94f3458d763da2
ThotaSujan <sujanthota@gmail.com>
2022-08-30 20:17:00 +0530

    Update streamlit_app.py
    added data menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,7 @@ import streamlit
 streamlit.title('I am ready to load the stream')
 streamlit.header ('This is my menu')
 streamlit.text('Orange juice')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
