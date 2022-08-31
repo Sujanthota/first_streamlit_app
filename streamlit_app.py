85e7641a2da3a8eb1625cd226f2a471181edf019
ThotaSujan <sujanthota@gmail.com>
2022-08-31 15:12:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit
 import pandas
 
-
 streamlit.title('I am ready to load the stream')
 streamlit.header ('This is my menu')
 streamlit.text('🍞 Orange juice')
@@ -11,9 +10,9 @@ streamlit.text(' 🐔 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥑 Hard-Boiled Free-Range Egg')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
+
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected= streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Cantaloupe'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
@@ -25,8 +24,8 @@ streamlit.write('The user entered ', fruit_choice)
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + " kiwi")
-
-
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
 # write your own comment -what does the next line do? 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
