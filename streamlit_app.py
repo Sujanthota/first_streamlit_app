ceab671facf50dc94397f71bd200ef499e0a661e
ThotaSujan <sujanthota@gmail.com>
2022-08-31 14:35:08 +0530

    Update streamlit_app.py
    requests

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit
 import pandas
 
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
+
 streamlit.title('I am ready to load the stream')
 streamlit.header ('This is my menu')
 streamlit.text('🍞 Orange juice')
