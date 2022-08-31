3df513b5426aad00fdb203d01aa979720eb131c5
ThotaSujan <sujanthota@gmail.com>
2022-08-31 17:01:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,17 +22,25 @@ fruits_selected= streamlit.multiselect("Pick some fruits:", list(my_fruit_list.i
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
+def get_fruityvice_data(this_fruit_choice):
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+       fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
+
 streamlit.header('Fruityvice Fruit Advice!')
+
 try:
     fruit_choice = streamlit.text_input('What fruit would you like information about?')
     if not fruit_choice:
          streamlit.error("Please select the fruit to get information")
     else:
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+        back_from_function = get_furityvice_data(fruit_choice)
+          streamlit.dataframe(back_from_function)
+        #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
     # write your own comment -what does the next line do? 
-        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+        #fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
   # write your own comment - what does this do?
-        streamlit.dataframe(fruityvice_normalized)
+      
 except URLError as e:
      streamlit.error()
 #streamlit.write('The user entered ', fruit_choice)
